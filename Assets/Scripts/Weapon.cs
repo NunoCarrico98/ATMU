bde2a073909f2d7a2d0cd65afac93a6de727da59
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-03-18 17:53:04 +0000

    Small Changes to some Scripts

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask notToHit;
+
+    float timeToFire = 0;
+    Transform firePoint;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("BRUHHH! There's no firepoint!!");
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //If weapon is single-burst
+		if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot ()
+    {
+
+    }
+}
