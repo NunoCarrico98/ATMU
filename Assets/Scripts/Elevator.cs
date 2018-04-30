8bc9a78c1c5f13c525841bc78444fd3da74affd1
Nuno Carriço <nuno.carrico.98@gmail.com>
2018-04-30 18:15:19 +0100

    Fix bug with PressurePlate

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float elevatorSpeed = 5f;
+    public bool pressured;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pressured = GetComponent<PressurePlate>().pressured;
+
+        
+    }
+}
--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -16,9 +16,6 @@ public class PressurePlate : MonoBehaviour
         anim = GetComponent<Animator>();
 
         pressured = false;
-
-        upPos = new Vector2(transform.position.x, transform.position.y);
-        downPos = new Vector2(transform.position.x, transform.position.y - 5);
     }
 
     private void Update()
@@ -33,35 +30,12 @@ public class PressurePlate : MonoBehaviour
             col.gameObject.tag == "Robot")
         {
             pressured = true;
-            IsPressured();
         }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         pressured = false;
-        IsPressured();
     }
 
-    private void IsPressured()
-    {
-        if (pressured)
-        {
-            MovePressurePlateDown();
-        }
-        else
-        {
-            MovePressurePlateUp();
-        }
-    }
-
-    private void MovePressurePlateDown()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, downPos, moveSpeed * Time.deltaTime);
-    }
-
-    private void MovePressurePlateUp()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, upPos, moveSpeed * Time.deltaTime);
-    }
 }
