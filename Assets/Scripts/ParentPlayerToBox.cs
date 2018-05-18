179286d84fa27a9523ba1fd512fb1b35f2dc8eae
Nuno Carriço <nuno.carrico.98@gmail.com>
2018-05-18 20:54:41 +0100

    Fix Boxes On Hooks. Change Door, PP, and CP1 position on level. Add new CP

--- Assets/Scripts/ParentPlayerToBox.cs
+++ Assets/Scripts/ParentPlayerToBox.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ParentPlayerToBox : MonoBehaviour {
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.transform.tag == "Player")
         {
-            col.transform.SetParent(transform);
+            col.transform.SetParent(transform.parent);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
         if (col.transform.tag == "Player")
         {
